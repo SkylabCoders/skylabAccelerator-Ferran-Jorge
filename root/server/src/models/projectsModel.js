f940d3291dd9699c378a699a10e6def7051e0a85
fbiayna <ferranbiaynacosta@gmail.com>
2020-11-21 01:32:15 +0100

    server url -atlas

--- root/server/src/models/projectsModel.js
+++ root/server/src/models/projectsModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-  name: { type: String },
+  name: { String },
 });
 
 module.exports = model('Projects', userSchema);
