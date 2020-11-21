7477bc44311cac227341bef57a8a58df8a20919b
Jorge <jorgengarriga@gmail.com>
2020-11-21 12:00:17 +0100

    Add all reducers from CRUD Actions

--- root/client/src/redux/reducers/projectsReducer.js
+++ root/client/src/redux/reducers/projectsReducer.js
@@ -2,17 +2,31 @@ import actionTypes from '../actions/actionsTypes';
 
 const initialState = {
   projectList: [],
+  createdProject: [],
+  updatedProject: [],
+  deletedProject: [],
   error: [],
 };
 
-export default function todosReducer(state = initialState, action) {
-  const { type, error, projectList } = action;
+export default function projectsReducer(state = initialState, action) {
+  const {
+    type, error, projectList, createdProject, updatedProject, deletedProject,
+  } = action;
   let updateState;
   switch (type) {
     case actionTypes.LOAD_PROJECTS_LIST:
       updateState = { ...state, projectList };
       break;
-    case actionTypes.ERROR_LOADING_PROJECT_LIST:
+    case actionTypes.CREATE_PROJECT:
+      updateState = { ...state, createdProject };
+      break;
+    case actionTypes.UPDATE_PROJECT:
+      updateState = { ...state, updatedProject };
+      break;
+    case actionTypes.DELETE_PROJECT:
+      updateState = { ...state, deletedProject };
+      break;
+    case actionTypes.ERROR_HANDLER:
       updateState = { ...state, error };
       break;
     default:
