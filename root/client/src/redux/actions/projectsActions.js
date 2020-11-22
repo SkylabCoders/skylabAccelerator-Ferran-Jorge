407f4328ca53f893e3511d68acdf6b38ad62ceae
Jorge <jorgengarriga@gmail.com>
2020-11-22 15:25:55 +0100

    Fix CRUD actions function

--- root/client/src/redux/actions/projectsActions.js
+++ root/client/src/redux/actions/projectsActions.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-debugger */
 import axios from 'axios';
 import actionTypes from './actionsTypes';
 
@@ -55,7 +54,6 @@ export function createProject(projectInfo) {
   return async (dispatch) => {
     try {
       const { data } = await axios.post(`${apiURL}${projectsEndpoint}`, { ...projectInfo });
-      console.log();
       dispatch(createProjectSuccess(data));
     } catch (error) {
       dispatch(handleError(error));
@@ -63,12 +61,11 @@ export function createProject(projectInfo) {
   };
 }
 
-export function updateProject(projectToUpdate) {
+export function updateProject(updatedProject) {
   return async (dispatch) => {
     try {
-      const demo = await axios.put(`${apiURL}${projectsEndpoint}`, { projectToUpdate });
-      console.log(demo);
-      dispatch(updateProjectSuccess(demo));
+      const { data } = await axios.put(`${apiURL}${projectsEndpoint}`, { ...updatedProject });
+      dispatch(updateProjectSuccess(data));
     } catch (error) {
       dispatch(handleError(error));
     }
@@ -78,8 +75,8 @@ export function updateProject(projectToUpdate) {
 export function deleteProject(projectToDelete) {
   return async (dispatch) => {
     try {
-      const { data } = await axios.delete(`${apiURL}${projectsEndpoint}`);
-      dispatch(deleteProjectSuccess(data), { ...projectToDelete });
+      const { data } = await axios.delete(`${apiURL}${projectsEndpoint}`, { projectToDelete });
+      dispatch(deleteProjectSuccess(data));
     } catch (error) {
       dispatch(handleError(error));
     }
